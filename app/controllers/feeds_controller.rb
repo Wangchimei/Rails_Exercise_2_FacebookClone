5c5c1baf289db85af0c7a655738b29582ddceb35
Chi-Mei Wang <kimmy0929@gmail.com>
2019-09-30 10:31:43 +0900

    implemented index and show page

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,10 +1,35 @@
 class FeedsController < ApplicationController
+  before_action :set_feed, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @feeds = Feed.all.order(created_at: :desc)
   end
 
   def new
+    @feed = Feed.new
   end
 
-  def edit
+  def create
+
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_feed
+    @feed = Feed.find(params[:id])
+  end
+
+  def feed_params
+    params.require(:feed).permit(:title, :content, :image)
   end
 end
