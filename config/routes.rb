a365fee600a80f3ea12ead752fe43543cd366ea7
Chi-Mei Wang <kimmy0929@gmail.com>
2019-09-30 14:31:54 +0900

    imstalled bcrypt, implemented user validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/edit'
   resources :feeds do
     collection do 
       post :confirm
---
+++ db/migrate/20190930050533_remove_image_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveImageFromUsers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :profile_image, :text
+  end
+end
